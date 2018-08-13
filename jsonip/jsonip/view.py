09bd5f6d544f28c77917ba2f80a05b6f596801a1
dellsun <dellsun2014@gmail.com>
2018-08-13 15:52:55 +0800

    update

--- jsonip/jsonip/view.py
+++ jsonip/jsonip/view.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+import json
  
 def hello(request):
     return HttpResponse("Hello world ! ")
 
 
 def jsonip(request):
+    result = {}
+
     if request.META.has_key('HTTP_X_FORWARDED_FOR'):
-        ip =  request.META['HTTP_X_FORWARDED_FOR']
+        result["ip"] =  request.META['HTTP_X_FORWARDED_FOR']
     else:
-        ip = request.META['REMOTE_ADDR']
-    return HttpResponse(ip)
+        result["ip"] = request.META['REMOTE_ADDR']
+
+    return HttpResponse(json.dumps(result))
